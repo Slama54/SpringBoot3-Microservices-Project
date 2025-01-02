fae5985a5e5a2de1e8a7496db9c071cd65a01350
wael <wael.slama.issat@gmail.com>
2025-01-02 17:26:02 +0100

    inter-service communication and test with Wiremock

--- order-services/src/main/java/com/wael/microservices/order/service/OrderService.java
+++ order-services/src/main/java/com/wael/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.wael.microservices.order.service;
 
+import com.wael.microservices.order.client.InventoryClient;
 import com.wael.microservices.order.dto.OrderRequest;
 import com.wael.microservices.order.model.Order;
 import com.wael.microservices.order.repository.OrderRepository;
@@ -12,17 +13,27 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private  final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public  void placeOrder(OrderRequest orderRequest){
-        // Map OrderRequest to Order object
-        Order order =  new  Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+       var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
 
-        // Save order to OrderRepository
-        orderRepository.save(order);
+       if (isProductInStock){
+           // Map OrderRequest to Order object
+           Order order =  new  Order();
+           order.setOrderNumber(UUID.randomUUID().toString());
+           order.setPrice(orderRequest.price());
+           order.setSkuCode(orderRequest.skuCode());
+           order.setQuantity(orderRequest.quantity());
+
+           // Save order to OrderRepository
+           orderRepository.save(order);
+
+       }else {
+           throw new RuntimeException("Product with skuCode"+orderRequest.skuCode()+" not in stock");
+       }
+
+
 
 
     }
--- order-services/src/test/java/com/wael/microservices/order/OrderServicesApplicationTests.java
+++ order-services/src/test/java/com/wael/microservices/order/OrderServicesApplicationTests.java
@@ -1,19 +1,21 @@
 package com.wael.microservices.order;
 
 
+import com.wael.microservices.order.stubs.InventoryClientStub;
 import org.junit.jupiter.api.BeforeEach;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.shaded.org.hamcrest.Matchers;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
+//@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServicesApplicationTests {
 	@ServiceConnection
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
@@ -40,6 +42,7 @@ class OrderServicesApplicationTests {
                      "quantity": 1
                 }
                 """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
 
 		var responseBodyString = RestAssured.given()
@@ -52,7 +55,7 @@ class OrderServicesApplicationTests {
 				.statusCode(201)
 				.extract()
 				.body().asString();
-		//assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+		//assertThat(responseBodyString, Matchers.is("order Placed Successfully"));
 
 
 	}
