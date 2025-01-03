ab48a74a36c882bcc8089943971b949e7aa5e420
wael <wael.slama.issat@gmail.com>
2025-01-04 00:14:59 +0100

    Aggregate Rest API in Gateway

--- api-gateway/src/main/java/com/wael/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/wael/gateway/routes/Routes.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.*;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 
 @Configuration
 public class Routes {
@@ -33,4 +34,30 @@ public class Routes {
                         HandlerFunctions.http("http://localhost:8082"))
                 .build();
     }
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+
+                .filter(setPath("/api-docs"))
+                .build();
+}
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+
 }
--- order-services/src/main/java/com/wael/microservices/order/OrderServicesApplication.java
+++ order-services/src/main/java/com/wael/microservices/order/OrderServicesApplication.java
@@ -2,10 +2,10 @@ package com.wael.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-@EnableFeignClients
+
 public class OrderServicesApplication {
 
 	public static void main(String[] args) {
